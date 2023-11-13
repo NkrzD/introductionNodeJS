3b0f2e9f6426ac22e353eea6f0296e091aed3531
Adrien Wyrwal <adri1959@hotmail.fr>
2023-11-13 16:56:01 +0000

    ajoutBDD

--- index.js
+++ index.js
@@ -6,9 +6,57 @@ const port = 3010;
 
 app.use(express.static('static'));
 
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+const users = express.Router();
+app.use('/users', users);
+
+users.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/user.html'));
+});
+
+users.get('/createContact/:name', async(req, res) => {
+  const user = await prisma.monsite_test.create({
+
+    data: {
+  
+      id: 1,
+  
+      email: req.params.name + "@gmail.com",
+  
+      name: req.params.name,
+  
+    },
+  
+  })
+})
+
+users.get('/createContact/:name', async(req, res) => {
+  const userByName = await prisma.monsite_test.findFirst({
+
+    where: {
+  
+      name: {
+  
+        startsWith: 'Ada'
+  
+      }
+  
+    },
+  
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
